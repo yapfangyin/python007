eca1e582d45cc9bd152a30d519c225ebfe0c7b07
yapfangyin <147800614+yapfangyin@users.noreply.github.com>
2024-01-26 16:15:35 +0800

    Update mainprog.py

--- mainprog.py
+++ mainprog.py
@@ -3,7 +3,7 @@ from companyEmp import Employee
 
 employee001  = Employee("Jane", "Tan", "M00123")
 employee002 = Employee("Jim", "Wong", "M00555")
-
+employee003 = Employee("Peter", "Lim", "M00558")
 print(employee001.first_name, employee001.last_name, employee001.emp_number)
 
 
